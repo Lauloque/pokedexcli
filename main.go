c80937de330b074313f99feb2839fdcda4bd4a1f
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-08-31 16:25:32 +0200

    scanner implementation

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		input := scanner.Text()
+
+		cleanInput := cleanInput(input)
+
+		fmt.Printf("Your command was: %s\n", cleanInput[0])
+	}
 }
