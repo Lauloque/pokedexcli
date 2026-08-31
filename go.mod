90eee8f07358beed7e216127c1fbcb3b389eec2f
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-08-31 13:45:49 +0200

    init go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Lauloque/pokedexcli
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
