4e3d671d86c45221ea0498f160da9825d6ac4b36
Loïc 'Lauloque' Dautry <loic.dautry@gmail.com>
2026-09-01 14:09:47 +0200

    moved repl logic to repl.go

--- main.go
+++ main.go
@@ -1,46 +1,10 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
 	cfg := &config{
 		commands: getCommands(),
 	}
 
-	for {
-		fmt.Print("Pokedex >")
-		if !scanner.Scan() {
-			if err := scanner.Err(); err != nil {
-				fmt.Println("Error reading input:", err)
-			}
-			break
-		}
-		input := scanner.Text()
-
-		cleanInput := cleanInput(input)
-
-		if len(cleanInput) == 0 {
-			fmt.Print("Please type a command. Type 'help' to see commands list\n")
-			continue
-		}
-		i := cleanInput[0]
-
-		command, exists := cfg.commands[i]
-		if exists {
-			err := command.callback(cfg)
-			if err != nil {
-				fmt.Println(err)
-			}
-			continue
-		} else {
-			fmt.Printf("Unknown Command: '%s'\n", i)
-			continue
-		}
-	}
+	startRepl(cfg)
 }
